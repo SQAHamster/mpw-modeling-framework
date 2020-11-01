98320d152784bd6880b42ce4e63eafe8c6d3758b
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-11-01 13:41:24 +0100

    Documentation: add feature diagram

---
+++ documentation/scripts/req-enrich-markdown.py
@@ -0,0 +1,16 @@
+import pyperclip
+import re
+
+print("Using clipboard text")
+markdown = pyperclip.paste()
+
+markdown = re.sub(r"([A-Z]+\-\d+)", r"**\1**", markdown)
+markdown = re.sub(r"As a developer / student", r"As a _developer_ / _student_", markdown)
+markdown = re.sub(r"As a (developer|student)", r"As a _\1_", markdown)
+markdown = re.sub(r"(MUST HAVE)", "![MUST HAVE](https://github.com/Fumapps/poly-hamster-simulator/blob/master/documentation/graphics/helpers/wiki_musthave.png)", markdown)
+markdown = re.sub(r"(SHOULD HAVE)", "![MUST HAVE](https://github.com/Fumapps/poly-hamster-simulator/blob/master/documentation/graphics/helpers/wiki_shouldhave.png)", markdown)
+markdown = re.sub(r"(NICE TO HAVE)", "![MUST HAVE](https://github.com/Fumapps/poly-hamster-simulator/blob/master/documentation/graphics/helpers/wiki_nicetohave.png)", markdown)
+markdown = re.sub(r"(\d+ ms|\d+ x \d+|write\(String\)|readNumber|readString|INTERNAL|TODO)", r"`\1`", markdown)
+
+pyperclip.copy(markdown)
+
