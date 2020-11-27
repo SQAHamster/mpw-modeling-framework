2192e041b1eb67cb8bbcd1a224b23b5731807540
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-11-27 14:50:49 +0100

    Generation: create Command/Query input-structures in readers

--- de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/CommandReader.java
+++ de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/CommandReader.java
@@ -1,9 +1,35 @@
 package components;
 
+import java.util.List;
+
+import org.eclipse.emf.mwe.core.WorkflowContext;
+import org.eclipse.emf.mwe.core.issues.Issues;
+import org.eclipse.emf.mwe.core.monitor.ProgressMonitor;
+
+import inputs.impl.InputsFactoryImpl;
+
 public class CommandReader extends MultiResourceReader {
 	
 	public CommandReader() {
 		super(".henshin", "src");
+	}
+	
+	@Override
+	protected void invokeInternal(WorkflowContext context, ProgressMonitor monitor, Issues issues) {
+		super.invokeInternal(context, monitor, issues);
+		
+		var inputs = InputsFactoryImpl.eINSTANCE.createHenshinCommandInputs();
+		var modules = inputs.getModules();
+		
+		String modelSlot = getModelSlot();
+		var objects = (List<?>)context.get(modelSlot);
+		
+		for (Object object : objects) {
+			var commandModel = (org.eclipse.emf.henshin.model.Module)object;
+			modules.add(commandModel);
+		}
+		
+		context.set(modelSlot + "Composition", inputs);
 	}
 
 }
--- de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/QueryReader.java
+++ de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/QueryReader.java
@@ -1,11 +1,37 @@
 package components;
 
+import java.util.List;
+
+import org.eclipse.emf.mwe.core.WorkflowContext;
+import org.eclipse.emf.mwe.core.issues.Issues;
+import org.eclipse.emf.mwe.core.monitor.ProgressMonitor;
+
 import de.unistuttgart.iste.sqa.mpw.modeling.queries.QueryDslStandaloneSetup;
+import de.unistuttgart.iste.sqa.mpw.modeling.queries.querydsl.Model;
+import inputs.impl.InputsFactoryImpl;
 
 public class QueryReader extends MultiResourceReader {
 
 	public QueryReader() {
 		super(".query", "src/queries");
 		new QueryDslStandaloneSetup().createInjectorAndDoEMFRegistration();
+	}
+	
+	@Override
+	protected void invokeInternal(WorkflowContext context, ProgressMonitor monitor, Issues issues) {
+		super.invokeInternal(context, monitor, issues);
+
+		var inputs = InputsFactoryImpl.eINSTANCE.createQueryInputs();
+		var models = inputs.getModels();
+		
+		String modelSlot = getModelSlot();
+		var objects = (List<?>)context.get(modelSlot);
+		
+		for (Object object : objects) {
+			var queryModel = (Model)object;
+			models.add(queryModel);
+		}
+		
+		context.set(modelSlot + "Composition", inputs);
 	}
 }
