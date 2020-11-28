f6ac85269c0e5c8491b53a86a4f93debd78450fb
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-11-28 21:10:08 +0100

    Modeling: model initTerritory

--- de.unistuttgart.iste.sqa.mpw.hamstersimulator.java/core/src/test/java/de/unistuttgart/hamster/InitTerritoryTransformationTest.java
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.java/core/src/test/java/de/unistuttgart/hamster/InitTerritoryTransformationTest.java
@@ -1,27 +1,26 @@
 package de.unistuttgart.hamster;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import de.unistuttgart.hamster.hamster.HamsterGame;
+import de.unistuttgart.hamster.util.GameStringifier;
 import org.junit.jupiter.api.Test;
 
 public class InitTerritoryTransformationTest {
 	
 	@Test
 	public void testInit3x5() {
-//		HamsterGame game = GameStringifier.createFromString("");
-//
-//		Territory t = game.getTerritory();
-//		var sut = new InitTerritoryTransformation(t, 5, 3);
-//		sut.execute();
-//		assertEquals(15, t.getCells().size());
-//
-//		String actual = GameStringifier.toString(game);
-//		assertEquals("     ;"
-//				   + "     ;"
-//				   + "     ;", actual);
+		HamsterGame game = GameStringifier.createFromString("");
+
+		var sut = game.getTerritory();
+		sut.initTerritory(5, 3, game.getCommandStack());
+
+		assertEquals(15, sut.getTiles().size());
+
+		String actual = GameStringifier.toString(game);
+		assertEquals("     ;"
+				   + "     ;"
+				   + "     ;", actual);
 	}
 
 
