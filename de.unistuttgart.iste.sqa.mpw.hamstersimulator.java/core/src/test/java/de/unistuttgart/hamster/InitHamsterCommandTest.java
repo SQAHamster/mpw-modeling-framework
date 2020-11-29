5c4a79a0a3a1de4db76a1f991becef9db0278fae
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-11-29 18:29:58 +0100

    Modeling: fix initHamster and write test

---
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.java/core/src/test/java/de/unistuttgart/hamster/InitHamsterCommandTest.java
@@ -0,0 +1,35 @@
+package de.unistuttgart.hamster;
+
+import de.unistuttgart.hamster.hamster.HamsterGame;
+import de.unistuttgart.hamster.mpw.Direction;
+import de.unistuttgart.hamster.mpw.Location;
+import de.unistuttgart.hamster.util.GameStringifier;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class InitHamsterCommandTest {
+	
+	@Test
+	public void testInitHamsterOn1x1_and4Grains() {
+		HamsterGame game = GameStringifier.createFromString("   ;" +
+															"   ;");
+
+		var sut = game.getDefaultHamster();
+		sut.initHamster(game.getTerritory(), locationOf(1, 1), Direction.SOUTH, 4, game.getCommandStack());
+
+		String actual = GameStringifier.toString(game);
+		assertEquals("   ;" +
+				     " v ;", actual);
+		assertEquals(4, sut.getGrains().size());
+	}
+
+	private Location locationOf(int column, int row) {
+		var location = new Location();
+		location.setColumn(column);
+		location.setRow(row);
+		return location;
+	}
+
+
+}
