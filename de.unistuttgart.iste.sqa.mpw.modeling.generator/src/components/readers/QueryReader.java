ec4a8f4825aa2eff62af81dad92267dc4827fa96
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-03 09:00:31 +0100

    Modeling: rename querydsl project (part 3)

--- de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/readers/QueryReader.java
+++ de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/readers/QueryReader.java
@@ -13,7 +13,7 @@ import behaviorInputs.impl.InputsFactoryImpl;
 public class QueryReader extends MultiResourceReader {
 
 	public QueryReader() {
-		super(".query", "src-modeling");
+		super(".query", "src");
 		new QueryDslStandaloneSetup().createInjectorAndDoEMFRegistration();
 	}
 	
