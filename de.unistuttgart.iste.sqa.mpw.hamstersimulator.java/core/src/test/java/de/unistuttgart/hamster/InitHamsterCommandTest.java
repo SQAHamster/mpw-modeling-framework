05dc08744c481cc1be12146e5f8f6b3b5a0aaeea
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-03 20:22:56 +0100

    Modeling: add initHamsterConstraints with parameters

--- de.unistuttgart.iste.sqa.mpw.hamstersimulator.java/core/src/test/java/de/unistuttgart/hamster/InitHamsterCommandTest.java
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.java/core/src/test/java/de/unistuttgart/hamster/InitHamsterCommandTest.java
@@ -1,5 +1,6 @@
 package de.unistuttgart.hamster;
 
+import de.unistuttgart.hamster.framework.CommandConstraintException;
 import de.unistuttgart.hamster.hamster.HamsterGame;
 import de.unistuttgart.hamster.mpw.Direction;
 import de.unistuttgart.hamster.mpw.Location;
@@ -7,6 +8,7 @@ import de.unistuttgart.hamster.util.GameStringifier;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class InitHamsterCommandTest {
 	
@@ -22,6 +24,17 @@ public class InitHamsterCommandTest {
 		assertEquals("   ;" +
 				     " v ;", actual);
 		assertEquals(4, sut.getGrains().size());
+	}
+
+
+	@Test
+	public void testInitHamster_invalidGrainCount() {
+		HamsterGame game = GameStringifier.createFromString(" ;");
+
+		var sut = game.getDefaultHamster();
+		assertThrows(CommandConstraintException.class, () -> {
+			sut.initHamster(game.getCommandStack(), game.getTerritory(), locationOf(0, 0), Direction.SOUTH, -1);
+		});
 	}
 
 	private Location locationOf(int column, int row) {
