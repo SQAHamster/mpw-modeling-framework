e40cade83d20ad692e26dc765b20ef3eb7c575aa
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-09 20:01:37 +0100

    Generation: in CommandReader validate henshin commands with OCL

--- de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/readers/CommandReader.java
+++ de.unistuttgart.iste.sqa.mpw.modeling.generator/src/components/readers/CommandReader.java
@@ -1,10 +1,19 @@
 package components.readers;
 
+import java.util.HashMap;
 import java.util.List;
 
+import org.eclipse.emf.common.util.Diagnostic;
+import org.eclipse.emf.common.util.URI;
+import org.eclipse.emf.ecore.EValidator;
+import org.eclipse.emf.ecore.util.Diagnostician;
+import org.eclipse.emf.henshin.model.HenshinPackage;
 import org.eclipse.emf.mwe.core.WorkflowContext;
 import org.eclipse.emf.mwe.core.issues.Issues;
 import org.eclipse.emf.mwe.core.monitor.ProgressMonitor;
+import org.eclipse.ocl.pivot.utilities.OCL;
+import org.eclipse.ocl.xtext.completeocl.CompleteOCLStandaloneSetup;
+import org.eclipse.ocl.xtext.completeocl.validation.CompleteOCLEObjectValidator;
 
 import behaviorInputs.impl.InputsFactoryImpl;
 
@@ -17,7 +26,8 @@ public class CommandReader extends MultiResourceReader {
 	@Override
 	protected void invokeInternal(WorkflowContext context, ProgressMonitor monitor, Issues issues) {
 		super.invokeInternal(context, monitor, issues);
-		
+
+		var validator = createOclValidator();
 		var inputs = InputsFactoryImpl.eINSTANCE.createHenshinCommandInputs();
 		var modules = inputs.getModules();
 		
@@ -26,10 +36,28 @@ public class CommandReader extends MultiResourceReader {
 		
 		for (Object object : objects) {
 			var commandModel = (org.eclipse.emf.henshin.model.Module)object;
-			modules.add(commandModel);
+			
+			var diagnostics = Diagnostician.INSTANCE.createDefaultDiagnostic(commandModel);
+			validator.validate(commandModel, diagnostics, new HashMap<Object, Object>());
+			
+			if (diagnostics.getSeverity() == Diagnostic.OK) {
+				modules.add(commandModel);
+			} else {
+				issues.addError(diagnostics.getMessage());
+				for (var child : diagnostics.getChildren()) {
+					issues.addError("> " + child.getMessage());
+				}
+			}
 		}
 		
 		context.set(modelSlot + "Composition", inputs);
+	}
+	
+	private static EValidator createOclValidator() {
+		OCL ocl = OCL.newInstance();
+		CompleteOCLStandaloneSetup.doSetup();
+		var uri = "platform:/resource/de.unistuttgart.iste.sqa.mpw.modeling.commands/src/CommandValidation.ocl";
+		return new CompleteOCLEObjectValidator(HenshinPackage.eINSTANCE, URI.createURI(uri), ocl.getEnvironmentFactory());
 	}
 
 }
