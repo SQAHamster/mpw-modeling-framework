64adee4db418a3efb1e588cae2d164d09b6a3fb3
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-21 18:47:13 +0100

    Java Simulator: add LocationTest
    Motivation: add tests for generated value types

---
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.java/core/src/test/java/de/unistuttgart/hamster/mpw/LocationTest.java
@@ -0,0 +1,39 @@
+package de.unistuttgart.hamster.mpw;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class LocationTest {
+    @Test
+    public void givenLocation0x0_whenCheckEqualsTo0x0_thenIsTrue() {
+        var sut = Location.from(0, 0);
+        boolean actual = sut.equals(Location.from(0, 0));
+        assertTrue(actual);
+    }
+
+    @Test
+    public void givenLocation1x1_whenCheckEqualsTo2x1_thenIsFalse() {
+        var sut = Location.from(1, 1);
+        boolean actual = sut.equals(Location.from(2, 1));
+        assertFalse(actual);
+    }
+
+    @Test
+    public void givenLocation3x5_whenPutIntoHashSet_andCheckIfSetContains3x5_thenCheckIsTrue() {
+        var hashSet = new HashSet<Location>();
+        hashSet.add(Location.from(3, 5));
+        boolean actual = hashSet.contains(Location.from(3, 5));
+        assertTrue(actual);
+    }
+
+    @Test
+    public void givenLocation3x5_whenPutIntoHashSet_andCheckIfSetContains3x3_thenCheckIsFalse() {
+        var hashSet = new HashSet<Location>();
+        hashSet.add(Location.from(3, 5));
+        boolean actual = hashSet.contains(Location.from(3, 3));
+        assertFalse(actual);
+    }
+}
