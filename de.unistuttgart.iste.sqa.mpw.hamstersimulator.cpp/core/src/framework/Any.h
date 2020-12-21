816a5c87c907d23bcfb63f5eec40bdd8fe846088
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-21 18:23:09 +0100

    C++ Generation: generate reflective accessors for enum / value-types

--- de.unistuttgart.iste.sqa.mpw.hamstersimulator.cpp/core/src/framework/Any.h
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.cpp/core/src/framework/Any.h
@@ -4,12 +4,14 @@
 #include <variant>
 #include <string>
 
-using EntityReference = void*;
+using RawReference = void*;
+using EntityReference = RawReference;
+using ValueReference = RawReference;
 using Any = std::variant<
         int,
         float,
         bool,
         std::string,
-        EntityReference>;
+        RawReference>;
 
 #endif //DE_UNISTUTTGART_ISTE_SQA_MPW_HAMSTERSIMULATOR_CPP_ANY_H
