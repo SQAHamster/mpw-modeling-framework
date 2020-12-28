226019a4b777c72b9c1d18c69d033d6f8b83e322
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-28 13:34:40 +0100

    Transformation: use Query-Expressions in Command-Model
    Motivation: for C++ is it necessary to have more type information, so
    the simple "getPropertyGetters" method will not work. To avoid another
    property-path model, the better way is to reuse the Query-Model
    Expression parsing and transformation logic in commands.
    
    Concrete use cases: AttributeConditions, Variables

---
+++ de.unistuttgart.iste.sqa.mpw.modeling.transformations/src/converter/StringToQueryDslConverter.java
@@ -0,0 +1,72 @@
+package converter;
+
+import java.io.StringReader;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.eclipse.m2m.qvt.oml.blackbox.java.Operation;
+import org.eclipse.m2m.qvt.oml.blackbox.java.Operation.Kind;
+import org.eclipse.xtext.parser.IParser;
+
+import com.google.inject.Guice;
+import com.google.inject.Injector;
+
+import de.unistuttgart.iste.sqa.mpw.modeling.querydsl.QueryDslRuntimeModule;
+import de.unistuttgart.iste.sqa.mpw.modeling.querydsl.querydsl.ClassContext;
+import de.unistuttgart.iste.sqa.mpw.modeling.querydsl.querydsl.Parameter;
+
+public class StringToQueryDslConverter {
+	/*
+	 * Due to: https://www.eclipse.org/forums/index.php/t/156231/
+	 * Use "Object" for complex parameter types and return type, since e.g. the usage of the Query-class will 
+	 * result in a compile error, since the QueryDsl package is not correctly recognized.
+	 */
+	@Operation(kind = Kind.HELPER)
+	public static Object toQuery(String string, String contextClassName, List<Object> parameterObjects) {
+		var parameters = parameterObjects.stream().map(Parameter.class::cast).collect(Collectors.toList());
+		var parser = createParser();
+		
+		String parseInputString = toParseString(string, contextClassName, parameters);
+		StringReader reader = new StringReader(parseInputString);
+		var result = parser.parse(reader);
+		
+		if (result.hasSyntaxErrors()) {
+            // TODO check how to handle this correctly, avoid exceptions in IDE
+			// throw new ParseException("String cannot be parsed to a query: " + result.getSyntaxErrors().toString());
+			return null;
+        }
+		return (ClassContext)result.getRootASTElement();
+	}
+
+	private static IParser createParser() {
+		Injector injector = Guice.createInjector(new QueryDslRuntimeModule());
+		var parser = injector.getInstance(IParser.class);
+		return parser;
+	}
+	
+	private static String toParseString(String string, String contextClassName, List<Parameter> parameters) {
+		var input = new StringBuilder();
+		input.append("context ");
+		input.append(contextClassName);
+		input.append(" query dummy");
+		if (parameters.size() > 0) {
+			input.append('(');
+			input.append(toParametersSignature(parameters));
+			input.append(')');
+		}
+		input.append(":");
+		input.append(string);
+		input.append(";");
+		return input.toString();
+	}
+	
+	private static String toParametersSignature(List<Parameter> parameters) {
+		return parameters.stream()
+				.map(p -> toNameTypeString(p))
+				.collect(Collectors.joining(", "));
+	}
+	
+	private static String toNameTypeString(Parameter parameter) {
+		return parameter.getVariableName() + ": " + parameter.getTypeName();
+	}
+}
