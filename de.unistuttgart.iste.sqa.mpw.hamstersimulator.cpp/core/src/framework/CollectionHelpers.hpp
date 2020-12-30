d3d795db49036c4966c9a2d8e08cb3a41eb7e2a5
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-30 21:19:10 +0100

    C++ Generation: generate expression helper methods
    Note: implement also type_select

--- de.unistuttgart.iste.sqa.mpw.hamstersimulator.cpp/core/src/framework/CollectionHelpers.hpp
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.cpp/core/src/framework/CollectionHelpers.hpp
@@ -2,6 +2,7 @@
 #define DE_UNISTUTTGART_ISTE_SQA_MPW_HAMSTERSIMULATOR_CPP_COLLECTION_HELPERS_H
 
 #include <list>
+#include <memory>
 #include <optional>
 
 namespace collectionhelpers {
@@ -13,6 +14,18 @@ namespace collectionhelpers {
       return *iter;
     }
     return std::nullopt;
+  }
+
+  template<typename TGT, typename SRC>
+  std::list<std::shared_ptr<TGT>> type_select(const std::list<std::shared_ptr<SRC>>& list) {
+      std::list<std::shared_ptr<TGT>> result;
+      for (auto element : list) {
+        auto targetElement = std::dynamic_pointer_cast<TGT>(element);
+          if (targetElement != nullptr) {
+              result.push_back(targetElement);
+          }
+      }
+      return result;
   }
 }
 
