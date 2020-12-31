94113bb5e1a070701a5132d11fe9f8dbbb1b9353
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2020-12-31 12:15:42 +0100

    C++ Generation: generate impl classes

---
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.cpp/core/src/impl/CMakeListsImpl.cmake
@@ -0,0 +1,15 @@
+set(IMPL_GEN_SRCS ${IMPL_GEN_SRCS}
+    "core/src/impl/SetPropertyCommandImpl.h"
+    "core/src/impl/SetPropertyCommandImpl.cpp"
+    "core/src/impl/AddEntityCommandImpl.h"
+    "core/src/impl/AddEntityCommandImpl.cpp"
+    "core/src/impl/RemoveEntityCommandImpl.h"
+    "core/src/impl/RemoveEntityCommandImpl.cpp"
+    "core/src/impl/CommandStackImpl.h"
+    "core/src/impl/CommandStackImpl.cpp"
+    "core/src/impl/GameViewPresenterImpl.h"
+    "core/src/impl/GameViewPresenterImpl.cpp"
+    "core/src/impl/GameLogImpl.h"
+    "core/src/impl/GameLogImpl.cpp"
+)
+    
