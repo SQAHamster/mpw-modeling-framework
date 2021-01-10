eb21698e28d0a1c6df32c5982de87380851212dc
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2021-01-10 21:50:04 +0100

    C++ Simulator: add HamsterView.h convenience api header

---
+++ de.unistuttgart.iste.sqa.mpw.hamstersimulator.cpp/core/src/impl/HamsterView.h
@@ -0,0 +1,12 @@
+#ifndef DE_UNISTUTTGART_ISTE_SQA_MPW_HAMSTERSIMULATOR_CPP_HAMSTERVIEW_H
+#define DE_UNISTUTTGART_ISTE_SQA_MPW_HAMSTERSIMULATOR_CPP_HAMSTERVIEW_H
+
+#include "GameViewPresenter.h"
+#include "GameViewModel.h"
+#include "ViewModelRow.h"
+#include "ViewModelCell.h"
+#include "ViewModelCellLayer.h"
+
+#include "GameViewPresenterImpl.h"
+
+#endif //DE_UNISTUTTGART_ISTE_SQA_MPW_HAMSTERSIMULATOR_CPP_HAMSTERVIEW_H
