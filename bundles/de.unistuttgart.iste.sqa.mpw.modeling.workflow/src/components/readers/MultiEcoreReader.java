0afd31d15ac6e37735431ce3f75c98bf80745631
Mario Fuksa <44242539+Fumapps@users.noreply.github.com>
2021-02-21 12:55:02 +0100

    Workflow: register loaded Ecore EPackages
    Motivation: previously, they were not registered correctly. This leads to reloading the packages by cross-references on other models.

--- bundles/de.unistuttgart.iste.sqa.mpw.modeling.workflow/src/components/readers/MultiEcoreReader.java
+++ bundles/de.unistuttgart.iste.sqa.mpw.modeling.workflow/src/components/readers/MultiEcoreReader.java
@@ -1,5 +1,11 @@
 package components.readers;
 
+import java.util.List;
+
+import org.eclipse.emf.ecore.EPackage;
+
+import util.ListLambdaVisitor;
+
 /**
  * MultiResourceReader Component used to read ecore files recursively in a given file path.
  */
@@ -7,6 +13,29 @@ public class MultiEcoreReader extends MultiResourceReader {
 
 	public MultiEcoreReader() {
 		super(".ecore", "model");
+	}
+	
+	@Override
+	protected void postProcessModels(List<Object> models) {
+		registerLoadedEcores(models);
+	}
+
+	private void registerLoadedEcores(List<Object> models) {
+		new ListLambdaVisitor<Object>()
+		.on(EPackage.class).then(ePackage -> {
+			validateNoPackageAlreadyRegistered(ePackage);
+			EPackage.Registry.INSTANCE.put(ePackage.getNsURI(), ePackage);
+		})
+		.orElse(object -> {
+			throw new IllegalStateException("loaded object is not of type EPackage: " + object);
+		}).accept(models);
+	}
+	
+	private void validateNoPackageAlreadyRegistered(EPackage ePackage) {
+		var registeredPackage = EPackage.Registry.INSTANCE.getEPackage(ePackage.getNsURI());
+		if (registeredPackage != null) {
+			throw new IllegalStateException("package " + ePackage.getNsURI() + " is already registered!");
+		}
 	}
 
 }
